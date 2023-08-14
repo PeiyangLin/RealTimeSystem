78b5f880fb627f771719fc55c5e45eed42da2daa
Peiyang <langaLinn@gmail.com>
2023-08-14 11:22:45 +0800

    dev 2023.08.14
    Signed-off-by: Peiyang <langaLinn@gmail.com>

--- DataCollector/DataCollector.py
+++ DataCollector/DataCollector.py
@@ -4,14 +4,21 @@ import pylsl
 import time
 
 
+def normalize(data):
+    mean = np.mean(data)
+    std = np.std(data)
+    data = (data - mean) / std
+    return data
+
+
 class DataCollector:
-    def __init__(self, C3=14, Eog=31, M2=18, fs=500, warmingTime=10):
+    def __init__(self, C3=14, Eog=31, M2=18, fs=500, warmingTime=30):
         # Init the fixed parameters
         self.C3 = C3
         self.Eog = Eog
         self.M2 = M2
         self.fs = fs
-        self.warmingTime = warmingTime * 60
+        self.warmingTime = int(warmingTime * 60)
 
         # first resolve an EEG stream on the lab network
         print("looking for an EEG stream...")
@@ -20,7 +27,7 @@ class DataCollector:
         # create a new inlet to read from the stream
         self.inlet = pylsl.StreamInlet(streams[0])
         self.chunk_C3 = []
-        self.chunk_REog = []
+        self.chunk_Eog = []
         self.chunk_M2 = []
 
         self.init_time = 0
@@ -35,25 +42,44 @@ class DataCollector:
                 sample, _ = self.inlet.pull_sample()
 
                 self.chunk_C3.append(sample[self.C3] / 1e6)
-                self.chunk_REog.append(sample[self.Eog] / 1e6)
+                self.chunk_Eog.append(sample[self.Eog] / 1e6)
                 self.chunk_M2.append(sample[self.M2] / 1e6)
+
+                self.Buffer()
 
         threading.Thread(target=func, args=()).start()
 
-    def warmDone(self):
-        if time.time() - self.init_time > self.warmingTime:
+    def Buffer(self):
+        if len(self.chunk_C3) <= (self.warmingTime * self.fs):
+            return
+        else:
             self.warming = True
-        return None
+            del self.chunk_C3[0]
+            del self.chunk_Eog[0]
+            del self.chunk_M2[0]
+
+    def Normalize(self):
+        Norm_C3 = np.array(self.chunk_C3)
+        Norm_EOG = np.array(self.chunk_Eog)
+        Norm_M2 = np.array(self.chunk_M2)
+
+        Norm_C3 = normalize(Norm_C3)
+        Norm_EOG = normalize(Norm_EOG)
+        Norm_M2 = normalize(Norm_M2)
+
+        return Norm_C3, Norm_EOG, Norm_M2
 
     def dataGet(self, NeedTime):
         ind = int(-NeedTime * self.fs)
-        data = [self.chunk_C3[ind:],
-                self.chunk_REog[ind:],
-                self.chunk_M2[ind:]]
+        Norm_C3, Norm_EOG, Norm_M2 = self.Normalize()
+        data = [Norm_C3[ind:],
+                Norm_EOG[ind:],
+                Norm_M2[ind:]]
         return np.array(data)
 
     def dataGet_All(self):
-        length = min(len(self.chunk_C3), len(self.chunk_REog), len(self.chunk_M2))
-        data = [self.chunk_C3[:length], self.chunk_REog[:length], self.M2[:length]]
+        length = min(len(self.chunk_C3), len(self.chunk_Eog), len(self.chunk_M2))
+        Norm_C3, Norm_EOG, Norm_M2 = self.Normalize()
+        data = [Norm_C3[:length], Norm_EOG[:length], Norm_M2[:length]]
 
         return np.array(data)
--- Main.py
+++ Main.py
@@ -5,7 +5,13 @@ import threading
 from DataCollector.DataCollector import DataCollector
 
 # Subject Init
+pass
 
+# Sleep Staging Model Init
+pass
+
+# Sleep Spindle Model Init
+pass
 
 # Channel Init
 C3 = 14
@@ -13,19 +19,13 @@ C4 = 16
 M2 = 18
 EOG = 31
 fs = 500
-warming = 10
+warming = 0.5
 
 # Data Collector Init & Start
 collector = DataCollector(C3, EOG, M2, fs, warming)
 collector.dataCollect()
 
-
-
-sleep = 3
 while True:
-    time.sleep(sleep)
-    temp = collector.dataGet(sleep)
-
-    temp = np.array(temp)
-    print(temp.shape)
-
+    time.sleep(5)
+    data = collector.dataGet_All()
+    print(data.shape)
