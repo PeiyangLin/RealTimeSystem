632d784d7fc04c4b771fb85aa957b4ef1822177e
Peiyang <langaLinn@gmail.com>
2023-11-07 22:41:08 +0800

    dev 2023.11.7 Fine version for BP & ANT
    Signed-off-by: Peiyang <langaLinn@gmail.com>

--- Main.py
+++ Main.py
@@ -1,31 +1,35 @@
 import time
-from DataCollector.DataCollector import DataCollector_EEG, DataCollector_EOG, DataCollector_ALL
-import SleepStageDetect
-from PortInput import StageInput, AudioInput, SlowOscillationInput
-from SODetect import SlowOscillationDetector
+from Utils.DataCollector.DataCollector import DataCollector
+from Utils import SleepStageDetect
+from Utils.PortInput import StageInput, AudioInput, SlowOscillationInput
+from Utils.SODetect import SlowOscillationDetector
 from Subjects import Subject
-import os
+
 
 # Channel Init
-Amplifier = "ANT"  # Amplifier device
+Amplifier = "BP"  # Amplifier device
 use_channel = 2  # EEG, EOG, EMG
 fs = 500  # Sampling Rate
-warming = 5  # Buffer Length
+warming = 10  # Buffer Length
+preNight_path = None
 
 # Staging Para
 stagingInterval = 3  # the interval of SO detect, the unit is s
 stagingDataLength = 30  # the data length when staging, the unit is s
 
+# Subject Init
+subject = Subject()
+logDir = subject.get_fileName()
+logPath = logDir + "/Subject_LOG.txt"
+log = open(logPath, "a+")
+
 # SO Para Init
+neg_thresh, pos_thresh, down_duration, up_duration = subject.subjectInit(preNight_path)
 wp = [0.5, 4]  # [low_freq_pass, high_freq_pass]
 ws = [0.01, 8]  # [low_freq_stop, high_freq_stop]
-neg_thresh = -40  # negative threshold
-pos_thresh = 15  # positive threshold
 filterOrder = 2  # the filter order
 detectPauseInterval = 1  # the interval of SO detect while find the up state, the unit is s
 detectInterval = 5  # the interval of SO detect, the unit is ms
-down_duration = 700
-up_duration = 700
 
 soDataLength = 30  # the data length when so detecting, the unit is s
 detectInterval = detectInterval / 1000
@@ -37,30 +41,16 @@ if Amplifier == "ANT":
     M2 = 18  # M2 channel index in amplifier
     EOG = 70  # EOG channel index in amplifier
     EMG = 71  # EMG channel index in amplifier
-    EMGREF = 0  # EMGREF channel index in amplifier
+    EMGREF = -1  # EMGREF channel index in amplifier
 elif Amplifier == "BP":
-    C3 = 4  # C3 channel index in amplifier
+    C3 = 4  # C3 channel ind ex in amplifier
     M2 = 31  # M2 channel index in amplifier
     EOG = 57  # EOG channel index in amplifier
     EMG = 52  # EMG channel index in amplifier
-    EMGREF = 0  # EMGREF channel index in amplifier
+    EMGREF = -1  # EMGREF channel index in amplifier
 else:
     pass
 
-# Subject Init
-subject = Subject()
-logDir = "LOG/%s" % subject.get_fileName()
-logPath = logDir + "/Subject_LOG.txt"
-isExists = os.path.exists(logDir)
-
-if not isExists:
-    os.mkdir(logDir)
-    os.mkdir(logDir + "")
-    os.mknod(logPath)
-log = open(logPath, "a+")
-log_info = subject.info
-for key in log_info:
-    log.write("%s: %s\n" % (key, log_info[key]))
 
 # Sleep Staging Model Init
 SleepStage = SleepStageDetect.SleepStageModel(useChannel=use_channel)
@@ -73,32 +63,22 @@ SO_Dector = SlowOscillationDetector(wp, ws, fs, neg_thresh, pos_thresh, down_dur
 
 # Port Writer
 StagePort = StageInput(device=Amplifier)
-# Audio Writer
+# Cue Writer
 SoundPort = AudioInput(device=Amplifier)
 # SO Writer
 SlowPort = SlowOscillationInput(device=Amplifier)
 
 # Data Collector Init & Start
-if use_channel == 1:
-    collector = DataCollector_EEG(C3, M2, fs, warming)
-elif use_channel == 2:
-    collector = DataCollector_EOG(C3, EOG, M2, fs, warming)
-elif use_channel == 3:
-    collector = DataCollector_ALL(C3, EOG, EMG, M2, fs, warming)
+collector = DataCollector(C3, EOG, EMG, M2, EMGREF, use_channel, Amplifier, fs, warming)
 collector.dataCollect()
 
 stage = "W"  # Init Stage
-N2_count = 0  # Init N2 Stage Count
-N3_count = 0  # Init N3 Stage Count
-R_count = 0  # Init REM Stage Count
-N2N3_count = 0
+stageCollector = SleepStageDetect.SleepStageCollector()
 SO_enable = True
 
 try:
     time.sleep(40)
-    start_point = time.time()
     now_stage = time.time()
-    now_audio = time.time()
     now_soEnable = time.time()
     now_soDetect = time.time()
 
@@ -110,33 +90,9 @@ try:
             data_Stage = collector.dataGet(stagingDataLength)
             stage, prob = SleepStage.predict(data_Stage)
             StagePort.writeStage(stage)
-            count_str = "No Counting"
+            count_str = stageCollector.update(stage)
 
-            if stage == "N2":
-                N2_count += 1
-                N2N3_count += 1
-                count_str = "N2 Count: %d" % N2_count
-            else:
-                N2_count = 0
-
-            if stage == "N3":
-                N3_count += 1
-                N2N3_count += 1
-                count_str = "N3 Count: %d" % N3_count
-            else:
-                N3_count = 0
-
-            if stage == "R":
-                R_count += 1
-                N2N3_count = 0
-                count_str = "REM Count: %d" % R_count
-            else:
-                R_count = 0
-
-            if stage == "W/N1":
-                N2N3_count = 0
-
-            # Audio Play during TMR
+            # Cue Play during TMR
             pass
 
             outputString = ("Stage: %s, Staging Prob: %s, StagingCount: %s, %s, %s\n" %
@@ -149,37 +105,33 @@ try:
             log.write(outputString)
 
         # SO Detect
-        if (N2N3_count >= 3) and SO_enable and (time.time() - now_soDetect >= detectInterval):
+        if (stageCollector.N2N3_count >= 3) and SO_enable and (time.time() - now_soDetect >= detectInterval):
             now_SO = time.time()
             data_SO = collector.dataGet_Single(soDataLength)
 
-            if N2_count > 0:
+            if stageCollector.N2_count > 0:
                 detectStage = "N2"
             else:
                 detectStage = "N3"
             SO_state = SO_Dector.SO_find(data_SO, detectStage)
-            SlowPort.writeSO(SO_state)
-            # print(SO_state)
 
             if SO_state == "UpState":
                 SO_enable = False
                 now_soEnable = time.time()
-                print("UP State, Use Time: %.3fms" % ((now_soEnable - now_SO) * 1000))
+                SlowPort.writeSO(SO_state, stage)
 
             now_soDetect = time.time()
-
-            # print("SO time: %.3f" % ((now_soDetect-now_SO) * 1000))
 
         # Reset the SO Detect Access
         if not SO_enable:
             if time.time() - now_soEnable >= detectPauseInterval:
                 SO_enable = True
 
-        # Audio Play
+        # Cue Play
         # if (time.time() - now_audio >= 5) and (time.time() - start_point <= 10 * 60):
         #     now_audio = time.time()
         #     soundPlay.audioPlay()
-        #     print("Audio Use Time: %.3fms" % ((time.time() - now_audio) * 1000))
+        #     print("Cue Use Time: %.3fms" % ((time.time() - now_audio) * 1000))
 
 except KeyboardInterrupt:
     log.close()
--- Subjects.py
+++ Subjects.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from Utils.Prepare.SlowWave_prepare import prepare
+import os
 
 
 class Subject:
@@ -7,14 +9,15 @@ class Subject:
     """
 
     def __init__(self):
-        self.name = "ZhouJianyang"
-        self.id = 1
-        self.gender = "Male"
-        self.age = 26
+        self.name = "JiangXuefei"
+        self.id = 33
+        self.gender = "Female"
+        self.age = 20
         self.info = {}
-        self.date = "2023_10_24"
+        self.date = "2023_10_26"
 
         self.__getDict__()
+        self.logDir = "LOG/%02d-%s-%s-%s" % (self.id, self.date, self.name, self.gender)
 
     def __getDict__(self):
         self.info = {"Name": self.name,
@@ -24,8 +27,7 @@ class Subject:
                      "Date": "%s" % self.date}
 
     def get_fileName(self):
-        fileName = "%02d-%s-%s-%s" % (self.id, self.date, self.name, self.gender)
-        return fileName
+        return self.logDir
 
     def show(self):
         self.__getDict__()
@@ -34,3 +36,23 @@ class Subject:
 
     def export(self):
         pass
+
+    def subjectInit(self, loadLabel=None):
+        isExists = os.path.exists(self.logDir)
+
+        if not isExists:
+            os.mkdir(self.logDir)
+            down_min_avg, up_max_avg, down_duration_avg, up_duration_avg = prepare(loadLabel, self.logDir)
+            self.info["Down_min_avg"] = down_min_avg
+            self.info["Up_max_avg"] = up_max_avg
+            self.info["Down_duration_avg"] = down_duration_avg
+            self.info["Up_duration_avg"] = up_duration_avg
+            info = pd.DataFrame(self.info)
+            info.to_csv(self.logDir + "/SubjectInfo.csv")
+        else:
+            info = pd.read_csv(self.logDir + "/SubjectInfo.csv")
+            down_min_avg = info["Down_min_avg"]
+            up_max_avg = info["Up_max_avg"]
+            down_duration_avg = info["Down_duration_avg"]
+            up_duration_avg = info["Up_duration_avg"]
+        return down_min_avg, up_max_avg, down_duration_avg, up_duration_avg
